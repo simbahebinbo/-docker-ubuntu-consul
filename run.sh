004a7137fa8f2f6764c7f04ebdde469850af0808
penghuaping <penghuaping@huobi.com>
2020-04-16 19:39:30 +0800

    修改参数传递

--- run.sh
+++ run.sh
@@ -2,16 +2,12 @@
 
 #启动consul程序
 
-#consul可执行文件
-CONSUL_BIN=$1
-#工作目录
-WORK_DIR=$2
 #consul的目录
-CONSUL_DIR=$3
+CONSUL_DIR=$1
 #应用程序名
-APP_NAME=$4
+APP_NAME=$2
 #consul集群的 server agent 链表
-CONSUL_SERVERS=$5
+CONSUL_SERVERS=$3
 #节点名
 NODE_NAME=consul-client-${APP_NAME}
 #网卡名
@@ -25,7 +21,7 @@ NODE_IP=$(ip -f inet address | grep inet | grep eth0 | awk '{print $2}' | awk -F
 # 只允许当前ip注册，即只允许该容器内的应用程序注册到该consul节点
 # 以 client 模式启动
 # 加入到 server 模式 的节点组成的集群
-EXEC_SCRIPT="${CONSUL_BIN} agent -bootstrap-expect=0 -advertise=${NODE_IP} -bind=${NODE_IP} -client=${LOCAL_IP} -node=${NODE_NAME} -data-dir=${CONSUL_DIR}/data -log-file=${CONSUL_DIR}/log/${APP_NAME}.log -log-level=INFO" 
+EXEC_SCRIPT="consul agent -bootstrap-expect=0 -advertise=${NODE_IP} -bind=${NODE_IP} -client=${LOCAL_IP} -node=${NODE_NAME} -data-dir=${CONSUL_DIR}/data -log-file=${CONSUL_DIR}/log/${APP_NAME}.log -log-level=INFO" 
 
 for consul_server in  $(echo ${CONSUL_SERVERS} | tr ',' ' ') 
 do
