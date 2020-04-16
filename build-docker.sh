d75ec3388ccc32465ca767d9c4deecb31c99fb4f
penghuaping <penghuaping@huobi.com>
2020-04-16 18:41:41 +0800

    修改ip

--- build-docker.sh
+++ build-docker.sh
@@ -2,4 +2,4 @@
 
 # 构建docker镜像
 
-docker build --build-arg CONSUL_NODES=172.26.110.125:8500,172.26.110.122:8500,172.26.111.116:8500,172.26.111.214:8500,172.26.111.41:8500 -t consul-consumer .
+docker build --build-arg CONSUL_NODES=consul-1.sinnet.huobiidc.com:8500,consul-2.sinnet.huobiidc.com:8500,consul-3.sinnet.huobiidc.com:8500,consul-4.sinnet.huobiidc.com:8500,consul-5.sinnet.huobiidc.com:8500,consul-6.sinnet.huobiidc.com:8500 -t consul-consumer .
